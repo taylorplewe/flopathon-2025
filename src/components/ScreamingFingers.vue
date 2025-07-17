d3c18dda01670fe78309c63aa332858dbad09111
scott-sutherland-awardco <scott.sutherland@awardco.com>
2025-07-17 11:48:27 -0600

    added team file

---
+++ src/components/ScreamingFingers.vue
@@ -0,0 +1,19 @@
+<script setup>
+import  VolumeDisplay  from "@/components/screaming-fingers/VolumeDisplay.vue";
+import ChangeVolumeButton  from "@/components/screaming-fingers/ChangeVolumeButton.vue";
+import TypingTest  from "@/components/screaming-fingers/TypingTest.vue";
+import VolumeConfirmation  from "@/components/screaming-fingers/VolumeConfirmation.vue";
+</script>
+
+<template>
+	  <div class="screaming-fingers">
+		<h1>Screaming Fingers</h1>
+		<VolumeDisplay />
+		<ChangeVolumeButton />
+		<TypingTest />
+		<VolumeConfirmation />
+	  </div>
+</template>
+
+<style scoped>
+</style>
