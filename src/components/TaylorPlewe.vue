c59df60d3f7576e82a7df62eb32100df3810b9da
Taylor Plewe <tplewe@outlook.com>
2025-07-17 10:42:01 -0600

    get basic slider to appear

--- src/components/TaylorPlewe.vue
+++ src/components/TaylorPlewe.vue
@@ -1,9 +1,35 @@
 <template>
-    <h1>t</h1>
+    <div id="container">
+        <div id="crank">
+        </div>
+
+        <div id="slider">
+            <div id="fill"></div>
+        </div>
+    </div>
 </template>
 
 <script setup>
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+#container {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    translate: -50% -50%;
+}
+#slider {
+    width: 400px;
+    height: 16px;
+    background-color: #eee;
+    border-radius: 4px;
+    position: relative;
+    overflow: hidden;
+    #fill {
+        position: absolute;
+        height: 100%;
+        background-color: #4b4;
+    }
+}
 </style>
